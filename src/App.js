0f915b612e623387bbcc9708f703237b11477657
Nathan Sharma <nathansharma007@gmail.com>
2025-02-15 15:55:51 -0600

    removed ok button, back to x

--- src/App.js
+++ src/App.js
@@ -70,9 +70,9 @@ function App() {
             <p className="mt-3 text-sm">GPA Calculator: Enter your grades in the following format: Letter grade(Number of semesters), separated by commas. For each letter grade, enter the number of semesters you earned that grade. For example, if I earned an A for 2 semesters, a B for 3 semesters, a C for 3 semesters, and a D for 0 semesters in all the 5.0 courses I have taken so far, I would enter A(2), B(3), C(3) in the KAP/AP placeholder. If you took high school credit courses in middle school, include those grades and semesters in your calculation.</p>
             <p className="mt-3 text-sm">Class Average Calculator: Input your grades separated by commas or their averages into each category. You can also use this as a what if calculator to see what you need to get the average you're aiming for. </p>
             <button
-              className="bg-blue-600 hover:bg-blue-900 text-white font-bold px-4 py-1 rounded mt-4"
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 font-bold text-xl"
               onClick={() => setShowHowToUse(false)}
-            > OK
+            > X
             </button>
           </div>
         </div>

85a704102ae2c4a62b2f8aecc2bb3417fa279de2
Nathan Sharma <nathansharma007@gmail.com>
2025-02-15 12:48:43 -0600

    made GPA calc instructions clearer

--- src/App.js
+++ src/App.js
@@ -69,11 +69,10 @@ function App() {
               className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
               onClick={() => setShowHowToUse(false)}
             >
-              X
             </button>
             <h2 className = "font-extrabold">How to use the calculators</h2>
             <p className="mt-3">Semester Calculator: Input your six weeks averages for any class into the SW fields. </p>
-            <p className="mt-3">GPA Calculator: Enter your grades in the following format: Letter grade(Number of semesters), separated by commas. For each letter grade, enter the number of semesters you earned that grade. For example, if you earned an A for 2 semesters, enter A(2). Put 'none' or leave the KAP/AP placeholder blank if you haven't taken any 5.0 courses. If you took high school credit courses in middle school, include those grades and semesters in your calculation.</p>
+            <p className="mt-3">GPA Calculator: Enter your grades in the following format: Letter grade(Number of semesters), separated by commas. For each letter grade, enter the number of semesters you earned that grade. For example, if I earned an A for 2 semesters, a B for 3 semesters, a C for 3 semesters, and a D for 0 semesters for a 5.0 course, I would enter A(2), B(3), C(3) in the KAP/AP placeholder.  the If you took high school credit courses in middle school, include those grades and semesters in your calculation.</p>
             <p className="mt-3">Class Average Calculator: Input your grades separated by commas or their averages into each category. You can also use this as a what if calculator to see what you need to get the average you're aiming for. </p>
           </div>
         </div>

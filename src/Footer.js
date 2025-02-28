ccf9f537d2a2e0b5c05909668c4c0a6bffe38fe9
Nathan Sharma <154014801+nathan-sharma@users.noreply.github.com>
2025-02-28 17:24:44 +0000

    edited footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-gray-100 text-gray-500 py-2 border-t border-gray-200 w-full text-center text-xs">
-      <p>A website by Nathan Sharma</p>
+      <p>Website by Nathan Sharma</p>
     </footer>
   );
 }

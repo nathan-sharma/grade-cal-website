1c01c73e326106a71cb65dd9782dd962ad12f84b
Nathan Sharma <154014801+nathan-sharma@users.noreply.github.com>
2025-04-24 22:56:38 +0000

    done

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import App from './App';
 import Feedback from './Feedback'; 
 import reportWebVitals from './reportWebVitals';
 import HowToUse from './HowToUse.js'
+import Thanks from './thanks.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,6 +21,7 @@ root.render(
         <Route path="/" element={<App />} /> 
         <Route path="/feedback" element={<Feedback/>} />
         <Route path = "/how-to-use" element ={<HowToUse/>}  />
+        <Route path = "/thanks" element ={< Thanks />}  />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

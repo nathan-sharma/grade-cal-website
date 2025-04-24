3c460b975b471c0d75869db8bcf5d3bfb162d3a0
Nathan Sharma <154014801+nathan-sharma@users.noreply.github.com>
2025-04-24 23:03:10 +0000

    fixed link

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer className="bg-gray-100 text-gray-500 py-2 border-t border-gray-200 w-full text-center text-xs">
-      <p>Website by Nathan Sharma</p>
+       <p>
+        Website by <Link to="/thanks">Nathan Sharma</Link>
+      </p>
     </footer>
   );
 }
--- src/thanks.js
+++ src/thanks.js
@@ -7,7 +7,7 @@ function Thanks() {
             <Navbar />
             <h2 className="text-2xl font-semibold mb-4 mt-10 text-center"> Love you Mom Dad and Nahnna!</h2>
             <h2 className="text-2xl font-semibold mb-4 mt-10 text-center"> Thank you Vinh for being my best friend for almost a decade, the 8 figures, and all my other friends who have been with me along the way </h2> 
-            <h2 className="text-2xl font-semibold mb-4 mt-10 text-center"> Also thanks to Ms. Cho for inspiring me to keep pushing forward</h2>
+            <h2 className="text-2xl font-semibold mb-4 mt-10 text-center"> Thanks to Ms. Cho for inspiring me to keep pushing forward</h2>
             <h2 className="text-2xl font-semibold mb-4 mt-10 text-center"> Miss you Pei wei</h2>
             <h2 className="text-2xl font-semibold mb-4 mt-10 text-center"> I honestly don't know where I would be without you guys</h2>
         <sm className = "text-center">4/24/25, freshman year</sm>

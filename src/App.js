0915c600f9d9e745c8ba8ac9d4b0ae09a173a5b5
Nathan Sharma <154014801+nathan-sharma@users.noreply.github.com>
2025-05-10 17:14:15 +0000

    added a first time visit popup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import SemesterCalculator from './SemesterCalculator';
 import GradeAverageCalculator from './GradeAverageCalculator';
 import GPACalculator from './GPACalculator';
@@ -5,21 +6,43 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 
 function App() {
+  const [showPopup, setShowPopup] = useState(false);
 
+  useEffect(() => {
+    const hasVisited = localStorage.getItem('hasVisited');
+    if (!hasVisited) {
+      setShowPopup(true);
+      localStorage.setItem('hasVisited', 'true');
+    }
+  }, []);
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
 
   return (
-    <div className="App flex flex-col items-center min-h-screen justify-start bg-gray-200 m-0"> 
-     <Navbar/>
-      <div className="mt-6 p-6 w-auto h-full"> 
+    <div className="App flex flex-col items-center min-h-screen justify-start bg-gray-200 m-0 realtive">
+      <Navbar />
+      {showPopup && (
+        <div className="fixed top-0 left-0 w-full h-full z-40 bg-black bg-opacity-50">
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-md shadow-lg z-100 border border-gray-300 text-center">
+          <p className="mb-4">👋 Hey there! Make sure you visit the <a href="/how-to-use" className="text-blue-500 underline">how to use</a> page before using our calculators!</p>
+          <button onClick={closePopup} className="bg-blue-600 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">
+            Got it!
+          </button>
+        </div>
+        </div>
+      )}
+      <div className="mt-6 p-6 w-auto h-full">
         <div className="flex flex-col space-y-8">
-        <div>
-        <GPACalculator />
+          <div>
+          <GradeAverageCalculator /> 
           </div>
           <div>
-          <GradeAverageCalculator />
+             <GPACalculator />
           </div>
           <div>
-          <SemesterCalculator /> 
+            <SemesterCalculator />
           </div>
         </div>
       </div>

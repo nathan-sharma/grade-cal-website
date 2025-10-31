eff2b01c8dff2f1883b09f8c83f4821d39833f15
Nathan Sharma <154014801+nathan-sharma@users.noreply.github.com>
2025-10-31 18:43:35 +0000

    minor popup change

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
         <div className="fixed top-0 left-0 w-full h-full z-40 bg-black bg-opacity-50">
         <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-md shadow-lg z-100 border border-gray-300 text-center">
           <p className="mb-4">👋 Hey there! Welcome to Katy Grade Calc, a completely free website that calculates your Katy ISD grades. Please make sure you visit the how to use page before using our calculators. Disclaimer: this website was not created nor endorsed by Katy ISD.</p>
-         <p className = "mb-4 font-bold">Calculators were updated on 10/25/2025.</p>
+         <p className = "mb-4 font-bold">Calculators are up to date for the 2025-26 KISD school year.</p>
           <button onClick={closePopup} className="bg-blue-600 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">
             Got it!
           </button>
